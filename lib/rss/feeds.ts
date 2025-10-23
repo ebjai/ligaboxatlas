807063ae1106887d1534babe3664133f39c568ac
ebjai <eddiebarberiz@gmail.com>
2025-10-22 22:37:36 -0400

    feat(rss): add boxing feeds

---
+++ lib/rss/feeds.ts
@@ -0,0 +1,9 @@
+export const BOXING_FEEDS = [
+  "https://www.boxingscene.com/rss",
+  "https://www.ringtv.com/feed/",
+  "https://www.badlefthook.com/rss/index.xml",
+  "https://www.dazn.com/en-US/news/boxing.rss",
+  "https://www.skysports.com/rss/12040",
+  "https://www.bbc.co.uk/sport/boxing/index.xml",
+  "https://www.espn.com/espn/rss/boxing/news",
+];
